0fead4626264de8b2a64832f4f22bb39fdc577cf
WilliamNg <williamngow2328@gmail.com>
2025-12-04 17:29:09 +0800

    golden

--- pkg/executor/join/hash_join_v1.go
+++ pkg/executor/join/hash_join_v1.go
@@ -114,7 +114,7 @@ type HashJoinV1Exec struct {
 }
 
 // Close implements the Executor Close interface.
-func (e *HashJoinV1Exec) Close() error {
+func (e *HashJoinV1Exec) Close() (err error) {
 	if e.closeCh != nil {
 		close(e.closeCh)
 	}
@@ -138,7 +138,11 @@ func (e *HashJoinV1Exec) Close() error {
 			channel.Clear(e.ProbeWorkers[i].joinChkResourceCh)
 		}
 		e.ProbeSideTupleFetcher.probeChkResourceCh = nil
-		terror.Call(e.RowContainer.Close)
+		util.WithRecovery(func() { err = e.RowContainer.Close() }, func(r any) {
+			if r != nil {
+				err = errors.Errorf("%v", r)
+			}
+		})
 		e.HashJoinCtxV1.SessCtx.GetSessionVars().MemTracker.UnbindActionFromHardLimit(e.RowContainer.ActionSpill())
 		e.waiterWg.Wait()
 	}
@@ -159,7 +163,12 @@ func (e *HashJoinV1Exec) Close() error {
 	if e.stats != nil {
 		defer e.Ctx().GetSessionVars().StmtCtx.RuntimeStatsColl.RegisterStats(e.ID(), e.stats)
 	}
-	err := e.BaseExecutor.Close()
+
+	IsChildCloseCalledForTest = true
+	childErr := e.BaseExecutor.Close()
+	if childErr != nil {
+		return childErr
+	}
 	return err
 }
 
