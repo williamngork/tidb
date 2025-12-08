df43cf9c6a660c66ed35b464ef24e55e7f6525dd
WilliamNg <williamngow2328@gmail.com>
2025-12-08 17:48:50 +0800

    test

--- pkg/planner/core/casetest/physicalplantest/physical_plan__HABITAT_test.go
+++ pkg/planner/core/casetest/physicalplantest/physical_plan__HABITAT_test.go
@@ -1,15 +1,11 @@
 package physicalplantest
 
 import (
-	"flag"
 	"testing"
 
 	"github.com/pingcap/tidb/pkg/testkit"
 	"github.com/pingcap/tidb/pkg/testkit/testdata"
-	"github.com/pingcap/tidb/pkg/testkit/testmain"
-	"github.com/pingcap/tidb/pkg/testkit/testsetup"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/goleak"
 )
 
 func TestRuleAggElimination4Join(t *testing.T) {
@@ -48,32 +44,4 @@ func getCascadesTemplateData() testdata.TestData {
 	var testDataMap = make(testdata.BookKeeper)
 	testDataMap.LoadTestSuiteData("testdata", "cascades_template", true)
 	return testDataMap["cascades_template"]
-}
-
-var testDataMap = make(testdata.BookKeeper)
-
-func TestCascades__HABITAT(m *testing.M) {
-	testsetup.SetupForCommonTest()
-	flag.Parse()
-	testDataMap.LoadTestSuiteData("testdata", "plan_suite")
-	opts := []goleak.Option{
-		goleak.IgnoreTopFunction("github.com/golang/glog.(*fileSink).flushDaemon"),
-		goleak.IgnoreTopFunction("github.com/bazelbuild/rules_go/go/tools/bzltestutil.RegisterTimeoutHandler.func1"),
-		goleak.IgnoreTopFunction("github.com/lestrrat-go/httprc.runFetchWorker"),
-		goleak.IgnoreTopFunction("go.etcd.io/etcd/client/pkg/v3/logutil.(*MergeLogger).outputLoop"),
-		goleak.IgnoreTopFunction("gopkg.in/natefinch/lumberjack%2ev2.(*Logger).millRun"),
-		goleak.IgnoreTopFunction("github.com/tikv/client-go/v2/txnkv/transaction.keepAlive"),
-		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
-	}
-
-	callback := func(i int) int {
-		testDataMap.GenerateOutputIfNeeded()
-		return i
-	}
-
-	goleak.VerifyTestMain(testmain.WrapTestingM(m, callback), opts...)
-}
-
-func GetPlanSuiteData() testdata.TestData {
-	return testDataMap["plan_suite"]
 }
