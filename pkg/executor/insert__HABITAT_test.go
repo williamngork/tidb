3b43296f554af17a82597fff3d562d66ea1dc60e
WilliamNg <williamngow2328@gmail.com>
2025-12-12 14:29:34 +0800

    test

--- pkg/executor/insert__HABITAT_test.go
+++ pkg/executor/insert__HABITAT_test.go
@@ -11,14 +11,12 @@ func TestInsertDuplicateToGeneratedColumns__HABITAT(t *testing.T) {
 	tk := testkit.NewTestKit(t, store)
 	tk.MustExec("use test")
 	tk.MustExec(`
-		CREATE TABLE t3 (
-		id int,
-		time1 datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		time2 datetime GENERATED ALWAYS AS (date_add(time1, interval 1 day)) VIRTUAL,
-		KEY idx (id, time2)
-		);
-		`)
-	tk.MustExec("insert into t3 set id = 1;")
-	tk.MustExec("update t3 set id = 2;")
-	tk.MustExec("admin check table t3;")
+CREATE TABLE tmv (
+  J1 json, J2 json GENERATED ALWAYS AS (j1) VIRTUAL,
+  UNIQUE KEY i1 ((cast(j1 as signed array))),
+  KEY i2 ((cast(j2 as signed array)))
+);		`)
+	tk.MustExec("insert into tmv set j1 = '[1]';")
+	tk.MustExec("insert ignore into tmv set j1 = '[1]' on duplicate key update j1 = '[2]';")
+	tk.MustExec("admin check table tmv;")
 }
