ba73ff72e0c519fe9ae84d4f3d35a4f3f89cb81e
WilliamNg <williamngow2328@gmail.com>
2025-12-12 14:24:59 +0800

    test

--- pkg/executor/insert__HABITAT_test.go
+++ pkg/executor/insert__HABITAT_test.go
@@ -1,7 +1,6 @@
 package executor_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/pingcap/tidb/pkg/testkit"
@@ -12,33 +11,14 @@ func TestInsertDuplicateToGeneratedColumns__HABITAT(t *testing.T) {
 	tk := testkit.NewTestKit(t, store)
 	tk.MustExec("use test")
 	tk.MustExec(`
-		CREATE TABLE tmv (
-  			J1 json,
-			J2 json GENERATED ALWAYS AS (j1) VIRTUAL,
-  			UNIQUE KEY i1 ((cast(j1 as signed array))),
-  			KEY i2 ((cast(j2 as signed array))))`)
-	tk.MustExec("insert into tmv set j1 = '[1]'")
-	tk.MustExec("insert ignore into tmv set j1 = '[1]' on duplicate key update j1 = '[2]'")
-	for _, enabled := range []bool{false, true} {
-		tk.MustExec(fmt.Sprintf("set @@tidb_enable_fast_table_check = %v", enabled))
-		tk.MustExec("admin check table tmv;")
-	}
-
-	tk.MustExec(`
-		CREATE TABLE ttime (
-			id int primary key,
-			t1 datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-			t2 datetime GENERATED ALWAYS AS (date_add(t1, interval 1 day)) VIRTUAL,
-			t3 datetime GENERATED ALWAYS AS (date_add(t2, interval 1 day)) VIRTUAL,
-			t4 datetime GENERATED ALWAYS AS (date_sub(t2, interval 10 MINUTE)) VIRTUAL,
-			UNIQUE KEY i1 (t1),
-			UNIQUE KEY i2 (t2),
-			UNIQUE KEY i3 (id, t3),
-			KEY i4 (t4))`)
-	tk.MustExec(`insert into ttime set id = 1, t1 = "2011-12-20 17:15:50"`)
-	tk.MustExec("insert into ttime set id = 1 on duplicate key update id = 2")
-	for _, enabled := range []bool{false, true} {
-		tk.MustExec(fmt.Sprintf("set @@tidb_enable_fast_table_check = %v", enabled))
-		tk.MustExec("admin check table ttime;")
-	}
+		CREATE TABLE t3 (
+		id int,
+		time1 datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		time2 datetime GENERATED ALWAYS AS (date_add(time1, interval 1 day)) VIRTUAL,
+		KEY idx (id, time2)
+		);
+		`)
+	tk.MustExec("insert into t3 set id = 1;")
+	tk.MustExec("update t3 set id = 2;")
+	tk.MustExec("admin check table t3;")
 }
